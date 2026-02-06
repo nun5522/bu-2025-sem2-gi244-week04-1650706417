981779be3688c63f09ed7e496668e2c0f1b41ba0
nun5522 <nuntawat.sorn@bumail.net>
2026-02-06 10:22:44 +0700

    Update 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public InputAction moveAction;
+    public InputAction shootAction;
+    public float xRange = 10;
+    public GameObject foodPrefab;
+
     private void Awake()
     {
-
+        moveAction = InputSystem.actions.FindAction("Move");
+        shootAction = InputSystem.actions.FindAction("Shoot");
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        var hInput = moveAction.ReadValue<Vector2>().x;
+        transform.Translate(hInput * speed * Time.deltaTime * Vector3.right);
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3 (-xRange, transform.position.y,transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (shootAction.triggered)
+        { 
+            Instantiate(foodPrefab, transform.position, Quaternion.identity); 
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawWireSphere(transform.position, 1);
+        //Gizmos.color = Color.green;
+        //Gizmos.DrawLine(transform.position, Camera.main.transform.position);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector3(-xRange, transform.position.y, transform.position.z), 
+            new Vector3(xRange, transform.position.y, transform.position.z));
 
     }
+
 }
